85c809b9795e01eac64ca0547066dd14cd60698a
Joe Ferris <jferris@thoughtbot.com>
2014-07-14 14:28:20 -0400

    Revert to Server.cpp from master

--- test.rb
+++ test.rb
@@ -2,6 +2,8 @@ require 'rubygems'
 require 'capybara-webkit'
 require 'benchmark'
 
+puts "Testing on #{Capybara::Driver::Webkit::VERSION}"
+
 Benchmark.bm do |x|
   x.report "Connection.new" do
     port = Capybara::Webkit::Connection.new.port
