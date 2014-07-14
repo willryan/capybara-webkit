a0c7c7156a2e24ccba2a41747aab5e4e8ea14204
Joe Ferris <jferris@thoughtbot.com>
2014-07-14 14:21:56 -0400

    Test localhost resolution and not port selection

--- src/Server.cpp
+++ src/Server.cpp
@@ -10,7 +10,7 @@ Server::Server(QObject *parent) : QObject(parent) {
 
 bool Server::start() {
   connect(m_tcp_server, SIGNAL(newConnection()), this, SLOT(handleConnection()));
-  return m_tcp_server->listen(QHostAddress::Any, 0);
+  return m_tcp_server->listen(QHostAddress::LocalHost, 4567);
 }
 
 quint16 Server::server_port() const {
